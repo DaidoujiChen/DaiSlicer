6c3a60edfd5d2519072d403ebd478d7c8b656c6b
DaidoujiChen <daidoujichen@gmail.com>
2016-07-07 13:46:00 +0800

    註解調整

--- DaiSlicer/DaiSlicer/DaiSlicer.m
+++ DaiSlicer/DaiSlicer/DaiSlicer.m
@@ -25,13 +25,9 @@
 #pragma mark - Private Class Method
 
 + (void)forClassMethod:(id)obj method:(SEL)selector byBlock:(id)block className:(NSString *)className {
-    
     Class aClass = NSClassFromString(className);
     Class metaClass = object_getClass(aClass);
-    
-    // 如果 class 名稱不同 比方 Object 與 Dai_Object 才需要 isa swizzling
-    // 而且兩個 class 的 size 必須相同才可以做這件事
-    //Class aClass = NSClassFromString(className);
+
     if (![NSStringFromClass([obj class]) isEqualToString:className] && class_getInstanceSize(object_getClass([obj class])) == class_getInstanceSize(metaClass)) {
         object_setClass(obj, metaClass);
     }
@@ -41,8 +37,6 @@
     const char *typeEncoding = method_getTypeEncoding(targetMethod);
     BOOL didAddMethod = class_addMethod(metaClass, selector, blockIMP, typeEncoding);
     
-    // 如果插失敗了 表示之前建立過 所以重新設定 selector 對應 blockIMP
-    // 並且刪除掉舊的 oldBlockIMP
     if (!didAddMethod) {
         Method method = class_getInstanceMethod(metaClass, selector);;
         IMP oldBlockIMP = method_getImplementation(method);
@@ -52,12 +46,11 @@
 }
 
 + (void)forInstanceMethod:(id)obj method:(SEL)selector byBlock:(id)block className:(NSString *)className {
-    
     Class aClass = NSClassFromString(className);
     
     // 如果 class 名稱不同 比方 Object 與 Dai_Object 才需要 isa swizzling
     // 而且兩個 class 的 size 必須相同才可以做這件事
-    //Class aClass = NSClassFromString(className);
+    // Class aClass = NSClassFromString(className);
     if (![NSStringFromClass([obj class]) isEqualToString:className] && class_getInstanceSize([obj class]) == class_getInstanceSize(aClass)) {
         object_setClass(obj, aClass);
     }
